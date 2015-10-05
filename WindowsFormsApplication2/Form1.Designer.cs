4678cfd0a59702e4b8da844a18773f59f7fb410e
Patrick Hao <haoxiangpeng123@gmail.com>
2015-10-05 22:31:24 +0800

    New

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(312, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(52, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "下载";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(184, 53);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 25);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "请输入提取码：";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(58, 102);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(369, 23);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(370, 48);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(57, 34);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "取消";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(321, 161);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(106, 15);
+            this.linkLabel1.TabIndex = 7;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "求是潮Box官网";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 185);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "求是潮Box文件下载";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private Label label2;
+        private ProgressBar progressBar1;
+        private Button button2;
+        private LinkLabel linkLabel1;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button2.Enabled = false;
+        }
+
+        public async Task<string> DownloadFile(string URL)
+        {
+
+            try
+            {
+                HttpWebRequest QSCbox = (HttpWebRequest)WebRequest.Create(URL);
+                HttpWebResponse qscBox = (HttpWebResponse)QSCbox.GetResponse();
+                //获取文件名
+                var tem = qscBox.Headers.ToString();
+                var n1 = tem.IndexOf("name=", StringComparison.Ordinal) + 6;
+                var n2 = tem.IndexOf("\"; fil", StringComparison.Ordinal);
+                var name = tem.Substring(n1, n2 - n1);
+                string filename = GetFilePath(name);
+
+
+
+                long totalBytes = qscBox.ContentLength;
+                progressBar1.Maximum = (int)totalBytes / 1024;
+                progressBar1.Minimum = 1;
+                if (qscBox.ContentLength != 0)
+                {
+                    // MessageBox.Show("文件存在");  
+                    Stream st = qscBox.GetResponseStream();
+                    Stream so = new FileStream(filename, FileMode.Create);
+                    long totalDownloadedByte = 0;
+                    byte[] by = new byte[1024];
+
+                    int osize = await st.ReadAsync(by, 0, @by.Length);
+                    //  int osize = st.ReadAsync(by, 0, @by.Length);
+                    while (osize > 0)
+                    {
+                        if (!dl)
+                        {
+                            textBox1.Text = "";
+                            progressBar1.Value = 1;
+                            return "cancle";
+                        }
+                        totalDownloadedByte = osize + totalDownloadedByte;
+                        // so.Write(by, 0, osize);
+                        await so.WriteAsync(by, 0, osize);
+                        osize = await st.ReadAsync(by, 0, @by.Length);
+                        progressBar1.PerformStep();
+                        //   osize = st.Read(by, 0, @by.Length);
+                    }
+                    MessageBox.Show("下载成功");
+                    so.Close();
+                    st.Close();
+
+                    return "success";
+                }
+                MessageBox.Show("文件不存在该文件！");
+                return "failed";
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("未知错误");
+                return "下载文件失败！" + e;
+            }
+        }
+
+        
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var ma = textBox1.Text;
+            var url = "http://box.zjuqsc.com/-" + ma;
+            button2.Enabled = true;
+            //    Thread downLoadThread;
+            //    downLoadThread = new Thread(new ThreadStart(TDownloadFile));
+            //    downLoadThread.Priority = ThreadPriority.Highest;
+            //    downLoadThread.Start();
+            ////    downLoadThread.Join();
+            //  GetFilePath("");
+
+            await DownloadFile(url);
+
+
+        }
+
+        public bool dl = true;
+        public string GetFilePath(string filename)
+        {
+            FileDialog filepath = new SaveFileDialog();
+            filepath.FileName = filename;
+            if (filepath.ShowDialog() == DialogResult.OK)
+            {
+                return filepath.FileName.ToString();
+            }
+            return "fales";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var result = MessageBox.Show("你确定要取消吗？", "注意", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes )
+            {
+                dl = false;
+            }
+
+            // MessageBox.Show("it works");
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://box.zjuqsc.com/");
+        }
+    }
+}
